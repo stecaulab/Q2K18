cb864d00df3d3e803590cac0143759795ffc7241
stecaulab <42800665+stecaulab@users.noreply.github.com>
2018-08-29 11:01:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php require_once 'connection.php' ?>
+<?php require_once 'header.php'; ?>
+  <div class="container">
+    <div class="card-deck">
+      <div class="card">
+        <div class="card-body card-custom" id="q1">
+          <p class="text-center edit-icon"><i class="fas fa-edit fa-10x"></i></p>
+          <h2>Qualità</h2>
+          <p class="card-text">
+            - Anagrafica Clienti.<br>
+            - Gestisci i controlli.<br>
+            - Anagrafica Prodotti.<br>
+          </p>
+          <p class="card-text text-center"><a class="badge badge-success" href="">Vai a Qualità</a></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body card-custom" id="nc1">
+          <p class="text-center edit-icon2"><i class="fas fa-times fa-10x"></i></p>
+          <h2>N.C.</h2>
+          <p class="card-text">
+            - Generare Non Conformità.<br>
+            - Reclami.<br>
+            - Costi.
+          </p>
+          <p class="card-text text-center"><a class="badge badge-danger" href="">Vai N.C.</a></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body card-custom" id="stat1">
+          <p class="text-center edit-icon3"><i class="fas fa-chart-pie fa-10x"></i></p>
+          <h2>Qualità</h2>
+          <p class="card-text">
+            - Statistica.<br>
+            - Genera Report<br>
+            - Grafici Andamento.
+          </p>
+          <p class="card-text text-center"><a class="badge badge-secondary" href="">Vai a Statistica</a></p>
+        </div>
+      </div>
+
+      </div>
+  </div>
+<?php require_once 'footer.php'; ?>
